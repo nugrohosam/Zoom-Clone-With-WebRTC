ef3eb286cf244cd07b14ad1ac5f1f594f146c431
Nugroho Samiyono <nugrohosamiyono@gmail.com>
2020-11-12 01:01:20 +0700

    Add | Heroku peerjs port 9000

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: 'https://nugi-peerjs.herokuapp.com',
-  port: '9000'
+  port: '32786'
 })
 
 const myVideo = document.createElement('video')
