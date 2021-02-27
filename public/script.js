5a1aec9d26343d68e5c86f3e4de23c6da9a97e6d
Nugroho Samiyono <nugrohosamiyono@gmail.com>
2021-02-27 14:22:45 +0700

    Fix | Peerjs

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://video-call.nugrohosamiyono.com')
+const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: PEER_HOST,
